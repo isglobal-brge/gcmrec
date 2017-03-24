d6ce827842debba6b800a0be5bd28f5049fa0f02
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-03-24 09:21:12 +0100

    changes in addCenTime to facilitate the tasks

--- R/gcmrec.R
+++ R/gcmrec.R
@@ -425,27 +425,25 @@ par(cex = oldcex)
 
 
 "addCenTime"<-
-function(datin)
+function(datin, id=1, time=2, event=3)
 {
  #
  # datin should have id,time,event variables
  # 
  
- ids <- unique(datin$id)
+ ids <- unique(datin[,id])
  datout <- NULL
  for (i in ids)
  {
-  thissubj <- datin[datin$id == i,,drop=FALSE]
+  thissubj <- datin[datin[,id], == i,,drop=FALSE]
   nrecs <- nrow(thissubj)
-  if (thissubj$event[nrecs] == 1)
+  if (thissubj[nrecs, event] == 1)
   {
    newrec <- thissubj[nrecs,]
-   newrec$time <- 0
-   newrec$event <- 0
+   newrec[, time] <- 0
+   newrec[, event] <- 0
    thissubj <- rbind(thissubj, newrec)
   }
-
-
   datout <- rbind(datout, thissubj)
  }
  datout
